6fbeb85fbe9299351847cd5818f53733bf16a0fc
kmfoysal <faisal.cri@gmail.com>
2023-01-07 01:32:18 +0600

    updated

--- models/Review.js
+++ models/Review.js
@@ -34,7 +34,7 @@ const ReviewSchema = new mongoose.Schema(
           type: String,
           required: true,
         },
-        replyDate: new Date()
+        // replyDate: new Date()
       },
     ],
   },
